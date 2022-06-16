11e697a1992d569b0887f8e1733c55ae8e754c87
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-06-16 08:34:43 -0300

    08-React LifeCycle (Alert pais duplicado)

--- 08-React-Estado-LifeCycle/homework/src/App.js
+++ 08-React-Estado-LifeCycle/homework/src/App.js
@@ -6,35 +6,39 @@ import Cards from './components/Cards';
 export default function App() {
   const [cities, setCities] = useState([]);
   const apiKey = '4ae2636d8dfbdc3044bede63951a019b'
-  function onSearch(ciudad) {
-    cities.forEach(city => {
-      if(city.name === ciudad) return alert('La ciudad ya se encuentra en pantalla')
-    });
-    
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-      .then(r => r.json())
-      .then((recurso) => {
-        if(recurso.main !== undefined){
-          const ciudad = {
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: recurso.main.temp,
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
-          };
-          setCities(oldCities => [...oldCities, ciudad]);
-        } else {
-          alert("Ciudad no encontrada");
-        }
-      });
 
-    }
+  function onSearch(ciudad) {
+    let toogle = false
+    cities.forEach(city=>{
+      if (city.name.toLowerCase() === ciudad.toLowerCase()) {
+        toogle = true
+      }})
+    
+      if(toogle) return alert('La ciudad ya se encuentra en pantalla')
+      else {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+        .then(r => r.json())
+        .then((recurso) => {
+          if(recurso.main !== undefined){
+            const ciudad = {
+              min: Math.round(recurso.main.temp_min),
+              max: Math.round(recurso.main.temp_max),
+              img: recurso.weather[0].icon,
+              id: recurso.id,
+              wind: recurso.wind.speed,
+              temp: recurso.main.temp,
+              name: recurso.name,
+              weather: recurso.weather[0].main,
+              clouds: recurso.clouds.all,
+              latitud: recurso.coord.lat,
+              longitud: recurso.coord.lon
+            };
+            setCities(oldCities => [...oldCities, ciudad]);
+          } else {
+            alert("Ciudad no encontrada");
+          }
+        })
+  }
 
   function onClose(id){
     setCities(oldCities => oldCities.filter(city => city.id !== id))
